448a883684fadc3f165b8192a5b33032cf39de9d
ArianAndres <arianavm2002@gmail.com>
2026-05-24 23:26:01 -0400

    Create python/generador-perfil-cli/main.py

---
+++ python/generador-perfil-cli/main.py
@@ -0,0 +1,17 @@
+# Generador de Perfiles
+print("=== Generador de Perfiles ===")
+nombre = input("Ingresa tu nombre: ")
+edad = int(input("Ingresa tu edad: "))
+ciudad = input("Ingresa tu ciudad: ")
+rol = input("Ingresa tu profesión/rol: ")
+
+print("--- Perfil Generado ---")
+print(f"Nombre: {nombre}")
+print(f"Edad: {edad}")
+print(f"Ciudad: {ciudad}")
+print(f"rol: {rol}")
+
+bio = input("Escribe una breve biografía sobre ti: ")
+print(f"Biografía: {bio}")
+
+print("¡Perfil guardado correctamente!")

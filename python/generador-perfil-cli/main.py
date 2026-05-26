82e05d87342052fbf4e4e91d4d62ac0c853019fc
ArianAndres <arianavm2002@gmail.com>
2026-05-25 23:14:22 -0400

    v1.1 calcula edad futura con variables int

--- python/generador-perfil-cli/main.py
+++ python/generador-perfil-cli/main.py
@@ -2,6 +2,7 @@
 print("=== Generador de Perfiles ===")
 nombre = input("Ingresa tu nombre: ")
 edad = int(input("Ingresa tu edad: "))
+edad_futura = edad + 5
 ciudad = input("Ingresa tu ciudad: ")
 rol = input("Ingresa tu profesión/rol: ")
 
@@ -13,5 +14,6 @@ print(f"rol: {rol}")
 
 bio = input("Escribe una breve biografía sobre ti: ")
 print(f"Biografía: {bio}")
+print(f"En 5 años, tendrás {edad_futura} años.")
 
 print("¡Perfil guardado correctamente!")

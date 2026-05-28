edac4c76b2565795aa929661ca92798a11042193
ArianAndres <arianavm2002@gmail.com>
2026-05-27 20:02:26 -0400

    feat: v2.0 usa .format() y strings multilínea

--- python/generador-perfil-cli/main.py
+++ python/generador-perfil-cli/main.py
@@ -1,19 +1,29 @@
 # Generador de Perfiles
-print("=== Generador de Perfiles ===")
+print("=== Generador de Perfiles v2.0 ===")
+
 nombre = input("Ingresa tu nombre: ")
 edad = int(input("Ingresa tu edad: "))
-edad_futura = edad + 5
 ciudad = input("Ingresa tu ciudad: ")
 rol = input("Ingresa tu profesión/rol: ")
-
-print("--- Perfil Generado ---")
-print(f"Nombre: {nombre}")
-print(f"Edad: {edad}")
-print(f"Ciudad: {ciudad}")
-print(f"rol: {rol}")
-
 bio = input("Escribe una breve biografía sobre ti: ")
-print(f"Biografía: {bio}")
-print(f"En 5 años, tendrás {edad_futura} años.")
 
-print("¡Perfil guardado correctamente!")
+edad_futura = edad + 5
+
+# Usando .format para mostrar el perfil
+perfil = """
+--- Perfil Generado con .format ---
+Nombre: {}
+Edad: {}
+Ciudad: {}
+Rol: {}
+Biografía: {}
+
+En 5 años, tendrás {} años.
+¡Perfil guardado correctamente!
+""".format(nombre, edad, ciudad, rol, bio, edad_futura)
+
+print(perfil)
+
+# Bonus: Usando f-strings para mostrar el perfil
+print("\n--- Mismo perfil con f-strings ---")
+print(f"Nombre: {nombre}\nEdad: {edad}\nCiudad: {ciudad}\nRol: {rol}\nBiografía: {bio}\nEn 5 años, tendrás {edad_futura} años.\n¡Perfil guardado correctamente!")

bd85a50190d505044da106be891d88021305e849
ArianAndres <arianavm2002@gmail.com>
2026-05-28 20:44:34 -0400

    v2.1 generador de perfiles

--- python/generador-perfil-cli/main.py
+++ python/generador-perfil-cli/main.py
@@ -9,10 +9,23 @@ bio = input("Escribe una breve biografía sobre ti: ")
 
 edad_futura = edad + 5
 
+# Usando slicing para mostrar partes del perfil
+primera_letra = nombre[0]
+ultima_letra = nombre[-1]
+primer_nombre = nombre[0:5]
+nombre_invertido = nombre[::-1]
+cantidad_letras = len(nombre)
+
 # Usando .format para mostrar el perfil
 perfil = """
 --- Perfil Generado con .format ---
 Nombre: {}
+Primera Letra = {}
+Última Letra = {}
+Primer Nombre = {}
+Nombre Invertido = {}
+Cantidad de Letras = {}
+
 Edad: {}
 Ciudad: {}
 Rol: {}
@@ -20,7 +33,7 @@ Biografía: {}
 
 En 5 años, tendrás {} años.
 ¡Perfil guardado correctamente!
-""".format(nombre, edad, ciudad, rol, bio, edad_futura)
+""".format(nombre, primera_letra, ultima_letra, primer_nombre, nombre_invertido, cantidad_letras, edad, ciudad, rol, bio, edad_futura)
 
 print(perfil)
 
